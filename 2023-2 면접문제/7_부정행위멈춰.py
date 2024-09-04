094e43dc2c726bf2d2f0bfe35605c8edc91e7b59
ZerGim <wldn2094@soongsil.ac.kr>
2024-09-04 14:17:57 +0900

    가로 세로중 1이 있을 때를 고려를 못함.

--- 2023-2 면접문제/7_부정행위멈춰.py
+++ 2023-2 면접문제/7_부정행위멈춰.py
@@ -15,9 +15,25 @@ if N == 1 and M == 2:
     print("1 2")
     exit()
 if N == 2 and M == 2:
-    print("2")
+    print("4")
     print("1 2")
-    print("2 1")
+    print("4 3")
+    exit()
+if N == 1:
+    print("2")
+    for i in range(M//2):
+        print("1",end=" ")
+        print("2",end=" ")
+    if M % 2 == 1:
+        print("1")
+    exit()
+if M == 1:
+    print("2")
+    for i in range(N//2):
+        print("1")
+        print("2")
+    if N % 2 == 1:
+        print("1")
     exit()
 print("4")
 idx = 0
